6bd65f113604743d12856b7623b45f52d7ec9189
jrtabucao10-cpu <jrtabucao10@gmail.com>
2026-06-22 12:46:21 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Connect your Supabase project here. The anon key is designed to be public;
+// database security is enforced by the policies in supabase-schema.sql.
+window.NESTLEDGER_CONFIG = {
+  supabaseUrl: 'https://ktydvtxsyeyheazdpltf.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0eWR2dHhzeWV5aGVhemRwbHRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMTA5MDksImV4cCI6MjA5NzU4NjkwOX0.-gCS8KRbgo6By80mtDlPF_CfjNxAkyo7LPk4-G0wNW4'
+};
